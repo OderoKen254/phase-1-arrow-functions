ed2161f4aa5e5f7d4a07a53cdbd1a13ad41c3eb3
Kennedy Ouma Odero <kennedyrogouma222@gmail.com>
2025-03-20 01:33:53 +0300

    coplete las on arrow functions

---
+++ index.js
@@ -0,0 +1,31 @@
+
+const divide = (a, b) => a / b;
+
+const mathDivision = () => {
+    let a = 2000;
+    let b =100;
+    return divide(a,b);
+}
+console.log(mathDivision()); 
+
+
+
+const square = (x) => x * x;
+
+const getProduct = () => {
+    let x = 2;
+    return square(x)
+}
+console.log(getProduct());
+
+
+const add = (a,b) => a+b;
+
+const mathSum = () => {
+    let a = 3;
+    let b = 4;
+
+    return add(a,b)
+}
+console.log(mathSum());
+
